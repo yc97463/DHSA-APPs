4c5abba2bf412f518190c444aba6a2d78329c6d0
yc97463 <youualan87@gmail.com>
2022-12-17 00:47:57 +0800

    feat(page/card): add card page with Card components

--- src/pages/card.js
+++ src/pages/card.js
@@ -1,22 +1,31 @@
 import React from "react";
 import { Card } from "antd";
 
-function SaCard(){
+function SaCard() {
   return (
     <div>
       <h1>This is card.</h1>
-      <Card title="Default size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
-      <p>Card content</p>
-      <p>Card content</p>
-      <p>Card content</p>
-    </Card>
-    <Card size="small" title="Small size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
-      <p>Card content</p>
-      <p>Card content</p>
-      <p>Card content</p>
-    </Card>
+      <Card
+        title="Default size card"
+        extra={<a href="#">More</a>}
+        style={{ width: 300 }}
+      >
+        <p>Card content</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </Card>
+      <Card
+        size="small"
+        title="Small size card"
+        extra={<a href="#">More</a>}
+        style={{ width: 300 }}
+      >
+        <p>Card content</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </Card>
     </div>
-  )
+  );
 }
 
 export default SaCard;
