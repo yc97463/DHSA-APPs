3b800b497855cae0644e3fe9d73fef7146fb9c3e
yc97463 <youualan87@gmail.com>
2022-12-17 15:22:54 +0800

    feat(card): add useState and useEffect to fetch APIs

--- src/pages/card.jsx
+++ src/pages/card.jsx
@@ -1,28 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card } from 'antd';
 
+const endpoint = 'https://api.dhsa.ndhu.edu.tw';
+
 function SaCard() {
+  const [userInfo, setUserInfo] = useState('');
+  useEffect(() => {
+    async function getUserInfo() {
+      const res = await fetch(endpoint + '/auth/token', {
+        headers: {
+          token: localStorage.getItem('token'),
+        },
+      }).then((res) => res.json());
+      setUserInfo(res);
+    }
+    if (!userInfo) {
+      getUserInfo();
+    }
+  }, []);
+  const [membership, setMembership] = useState('');
+  useEffect(() => {
+    async function getMembership() {
+      const res = await fetch(
+        endpoint + '/card/membership/' + userInfo.email
+      ).then((res) => res.json());
+      setMembership(res);
+    }
+    if (!membership) {
+      getMembership();
+    }
+  }, []);
+  const [schoolStatus, setSchoolStatus] = useState('');
+  useEffect(() => {
+    async function getSchoolStatus() {
+      const res = await fetch(endpoint + '/auth/ndhuLDAP', {
+        headers: {
+          token: localStorage.getItem('token'),
+        },
+      }).then((res) => res.json());
+      setSchoolStatus(res);
+    }
+    if (!schoolStatus) {
+      getSchoolStatus();
+    }
+  }, []);
+
   return (
     <div>
       <h1>This is a card.</h1>
       <Card
-        title="Default size card"
+        title={userInfo.name}
         extra={<a href="#">More</a>}
         style={{ width: 300 }}
       >
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>Card content</p>
-      </Card>
-      <Card
-        size="small"
-        title="Small size card"
-        extra={<a href="#">More</a>}
-        style={{ width: 300 }}
-      >
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>Card content</p>
+        <p>Name: {userInfo.name}</p>
+        <p>ID: {userInfo.email}</p>
+        <p>Membership: {membership.membership}</p>
+        <p>Status: {schoolStatus.status}</p>
       </Card>
     </div>
   );
