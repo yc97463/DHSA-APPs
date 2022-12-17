7ff90987b1b503c413f535b0f97e754f76f55b4f
yc97463 <youualan87@gmail.com>
2022-12-17 12:44:58 +0800

    feat(home): add open card route link

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
-  return <h1>Hello, World</h1>;
+  return (
+    <div>
+      <h1>Hello, World</h1>
+      <p>
+        <Link to="/card">open Card</Link>
+      </p>
+    </div>
+  );
 }
 
 export default Home;
