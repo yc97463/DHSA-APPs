270355166bda50000ee65abcc7a6f69f99a26f54
yc97463 <youualan87@gmail.com>
2022-12-17 14:51:33 +0800

    feat(layout): add is_fullscreen

--- src/_layout/layout.jsx
+++ src/_layout/layout.jsx
@@ -1,18 +1,45 @@
 import React from 'react';
 import { Outlet } from 'react-router';
-import { Layout } from 'antd';
+import { Layout, Menu } from 'antd';
+import { Link, useSearchParams } from 'react-router-dom';
+import '../style.css';
 
 const { Header, Footer, Content } = Layout;
 
 function App() {
-  return (
-    <Layout>
-      <Header>Header</Header>
-      <Content>
-        <Outlet />
-      </Content>
-      <Footer>Footer</Footer>
-    </Layout>
-  );
+  let hasToken = localStorage.getItem('token') != null ? true : false;
+  const [searchParams] = useSearchParams();
+  const is_fullscreen = searchParams.get('fs') == null ? false : true;
+  if (is_fullscreen) {
+    return (
+      <Layout>
+        <Content>
+          <Outlet />
+        </Content>
+      </Layout>
+    );
+  } else {
+    return (
+      <Layout>
+        <Header>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            // items={items1}
+            className="nav"
+          >
+            <Link to="/">Home</Link>
+            <Link to={hasToken ? '/auth?logout' : '/auth'}>
+              {hasToken ? 'Logout' : 'Login'}
+            </Link>
+          </Menu>
+        </Header>
+        <Content>
+          <Outlet />
+        </Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    );
+  }
 }
 export default App;
