10d22144da583d93d5fb7e43bbfd59aeef2b47ff
yc97463 <hi@imych.one>
2023-04-01 17:40:56 +0800

    feat(next.config): add redirect rules for /room to rpage

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/room',
+        destination: 'https://dhsa.ndhu.edu.tw/app/index.php?Plugin=school&Action=schoolresapp&Res=78',
+        permanent: false,
+        basePath: false
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
